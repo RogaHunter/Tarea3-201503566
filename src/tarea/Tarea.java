c69f6d44391a1ec026470d287685a720186fdd87
ROGA11 <ROGA11@192.168.1.6>
2018-02-23 13:16:48 -0600

    estoy trabajando en mi tarea

--- src/tarea/Tarea.java
+++ src/tarea/Tarea.java
@@ -1,4 +1,6 @@
 package tarea;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  *
@@ -11,6 +13,48 @@ public class Tarea {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner sn = new Scanner(System.in);
+        boolean salir = false;
+        int opcion; //Guardaremos la opcion del usuario
+ 
+        
+ 
+            System.out.println("1. Usuarios");
+            System.out.println("2. Contador de Digitos");
+            System.out.println("3. Tres numeros de mayor a menor");
+            System.out.println("4. Calcular Promedio");
+            System.out.println("5. Salir");
+ 
+            try {
+ 
+                System.out.println("Elija una opcion");
+                opcion = sn.nextInt();
+ 
+                switch (opcion) {
+                    case 1:
+                        System.out.println("Has seleccionado Usuarios");
+                    break;
+                    case 2:
+                        System.out.println("Has seleccionado Contador de digitos");
+                    break;
+                    case 3:
+                        System.out.println("Has seleccionado Tres numeros de mayor a menor");
+                    break;
+                    case 4:
+                        System.out.println("Has Seleccionado Calcular Promedio");
+                    break;
+                    case 5:
+                        salir = true;
+                    break;
+                    default:
+                        System.out.println("Elegir solo entre 1 y 5");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Debes insertar un número");
+                sn.next();
+            }
+        
+ 
     }
     
 }
